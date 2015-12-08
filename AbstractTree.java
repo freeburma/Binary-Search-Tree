21de78fd1c97ac12d3a0b6bc0030973405d1372c
freeburma <kophyoblue@gmail.com>
2015-12-08 13:19:03 +1300

    Create AbstractTree.java

---
+++ AbstractTree.java
@@ -0,0 +1,32 @@
+/*
+ 	Project Name 		: 	Binary Search Tree (Assignment 3)
+ 	
+ 	Project Description :  
+ 	*/
+
+public abstract class AbstractTree <E extends Comparable <E>>
+									  implements Tree <E>
+{
+	// InOrder Traverse
+	@Override 
+	public void inOrder ()
+	{
+		
+	}
+	
+	// postOrder Traverse
+	@Override 
+	public void postOrder ()
+	{
+		
+	}
+	
+	// preOrder Traverse
+	@Override 
+	public void preOrder ()
+	{
+		
+	}
+	
+
+}
